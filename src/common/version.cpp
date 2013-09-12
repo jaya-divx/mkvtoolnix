e2c03e775c67c2d9f204353e4c779840ab58774e
mikecancilla-divx <michael.cancilla@rovicorp.com>
2013-09-12 10:28:06 -0700

    Version 1.0.5
    Fixed a bug with the handling of the VUI during SPS parsing.  We would output sar_width and sar_height whether or not the aspect_ratio_idc was EXTENDED_SAR.  See section E.1.1 of the HEVC spec.

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -25,7 +25,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-#define VERSIONNAME "Promised Land" " Rovi v1.0.4"
+#define VERSIONNAME "Promised Land" " Rovi v1.0.5"
 
 version_number_t::version_number_t()
   : valid(false)
